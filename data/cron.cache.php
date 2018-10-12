f86d68833c42df42799afe212e64704d9fbf07a9
xuxifan <cafan@126.com>
2018-10-12 13:58:03 +0800

    first commit

---
+++ data/cron.cache.php
@@ -0,0 +1,22 @@
+<?php
+$m_cron = array (
+  'information' => 
+  array (
+    'lastrun' => '1539303844',
+    'nextrun' => '1539360000',
+    'day' => '1',
+  ),
+  'advertisement' => 
+  array (
+    'lastrun' => '1539303844',
+    'nextrun' => '1539360000',
+    'day' => '1',
+  ),
+  'levelup' => 
+  array (
+    'lastrun' => '1539303844',
+    'nextrun' => '1539360000',
+    'day' => '1',
+  ),
+);
+?>
